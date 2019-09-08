2d7631a61b4f43f53e9ec1dc6d7eebbae7a62d5d
Luis <luigi_789@hotmail.com>
2019-09-07 23:47:05 -0400

    Maps working and getting my location

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -1,8 +1,9 @@
-import 'dart:async';
 import 'dart:core';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MyHomePage extends StatefulWidget {
 	@override
@@ -10,11 +11,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-	Completer<GoogleMapController> _completer = Completer();
-	static const LatLng _center = const LatLng(27.918325, -82.341408);
+	GoogleMapController googleMapController;
+	Geolocator geolocator = Geolocator();
+	Position userLocation;
+	CameraUpdate cameraUpdate;
+	CameraPosition cameraPosition = new CameraPosition(
+		target: LatLng(40.387019, -105.668516),
+		zoom: 5.0
+	);
 
-	void _onMapCreated(GoogleMapController mapController) {
-		_completer.complete(mapController);
+	static const LatLng _center = const LatLng(27.918325, -82.341408);
+	
+	Future<Position> initialPosition() async {
+		Position currentLocation;
+		try {
+			currentLocation = await geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+		}
+		catch(e) {
+			print(e);
+			currentLocation = null;
+		}
+		print(currentLocation);
+		getMyLocation(currentLocation.latitude, currentLocation.longitude);
+		return currentLocation;
+	}
+
+	getMyLocation(double lat, double long) {
+		print('getMyLocation()');
+		googleMapController.animateCamera(CameraUpdate.newCameraPosition(
+			CameraPosition(
+				target: LatLng(lat, long),
+				zoom: 15.0
+			)
+		));
 	}
 
 	Set<Marker> myMarkers = Set.from([
@@ -25,11 +54,52 @@ class _MyHomePageState extends State<MyHomePage> {
 		)
 	]);
 
+	void _onMapCreated(controller) {
+		setState(() {
+			googleMapController = controller;
+		});
+	}
+
+	int _selectedIndex = 0;
+
+	void _onItemTapped(int index) {
+		setState(() {
+			_selectedIndex = index;
+		});
+	}
+
+	resetCamera() {
+		print('resetCamera()');
+		googleMapController.animateCamera(CameraUpdate.newCameraPosition(
+			CameraPosition(
+				target: LatLng(40.387019, -105.668516),
+				zoom: 15.0
+			)
+		));
+	}
+
 	@override
 	Widget build(BuildContext context) {
 		return Scaffold(
 			appBar: AppBar(
-				title: Text('TransFLO'),
+				title: Text('TRANSFLO'),
+				actions: <Widget>[
+					IconButton(
+						onPressed: initialPosition,
+						icon: Icon(Icons.my_location),
+						alignment: Alignment.center,
+					),
+					IconButton(
+						onPressed: resetCamera,
+						icon: Icon(Icons.add_location),
+						alignment: Alignment.center,
+					),
+					IconButton(
+						onPressed: () {},
+						icon: Icon(Icons.search),
+						alignment: Alignment.center,
+					)
+				],
 			),
 			body: GoogleMap(
 				onMapCreated: _onMapCreated,
@@ -38,6 +108,24 @@ class _MyHomePageState extends State<MyHomePage> {
 					zoom: 11.0
 				),
 				markers: myMarkers
+			),
+			bottomNavigationBar: BottomNavigationBar(
+				items: <BottomNavigationBarItem>[
+					BottomNavigationBarItem(
+						icon: Icon(Icons.home),
+						title: Text('Home')
+					),
+					BottomNavigationBarItem(
+						icon: Icon(Icons.list),
+						title: Text('List')
+					),
+					BottomNavigationBarItem(
+						icon: Icon(Icons.history),
+						title: Text('History')
+					)
+				],
+				onTap: _onItemTapped,
+				currentIndex: _selectedIndex,
 			),
 		);
 	}
